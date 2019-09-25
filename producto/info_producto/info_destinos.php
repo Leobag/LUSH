20fba9410bfd700da6912c47825bb48d2c613ebc
Leobag <leonard.bagiu@gmail.com>
2019-09-25 16:19:23 -0300

    Final sprint 1

---
+++ producto/info_producto/info_destinos.php
@@ -0,0 +1,40 @@
+<?php
+$destinos = [
+
+"maldivas" => [
+
+  "destino" => "Las Maldivas",
+"bestseason" => "Desde Noviembre hasta Marzo",
+"destinos" => 2,
+"tiempoideal" => "5 Noches",
+"hospedaje" => "Constance Halaveli",
+"lugar" => "45 minutos en barco de Malé",
+"infolugar" => "Hemos eligido Constance Halaveli como el hotel
+ideal para quedarse en Las Maldivas. Se van a quedar sobre el
+agua en las halucinantes casas sobre el agua, con servicio de gimnasio,
+piletas privadas, un spa, buseo, tres opciones de cena cada dia ya ademas
+cortes de ténis. Cada casa viene equipado con televisór, Wi-Fi y cargadores de celular.
+Ademas todo esto hay posibilidades de irse con muchas excursiones cerca al hotel.
+Los mas populares son: irse a otra isla para probar surfear, busear cerca los arrecifes de Las Maldivas y visitar restaurantes en islas distintas.",
+
+"incluido" => ["Transporte del aeropuerto al hotel", "Chip de celular local con 20GB de surf"],
+
+"adicional" => ["Jet Privado", "Viaje privado desde tu casa al hotel", "Actividades del hotel"],
+
+"jetprivado" => ["Masaje", "Champagne", "Cena de 3 Pasos", "Cambio de ropa para dormir", "Un peluche"],
+
+
+],
+
+];
+
+
+
+
+
+
+
+
+
+
+ ?>
--- producto/maldivas.php
+++ producto/maldivas.php
@@ -1,9 +1,13 @@
 <?php
 
+include("info_producto/info_destinos.php");
+
+if($destinos["maldivas"]){
+  $infotrip = $destinos["maldivas"];
  ?>
 
 
-<html lang="en" dir="ltr">
+<html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,15 +18,58 @@
     <link rel="stylesheet" href="css/allstyles.css">
     <title>Viajá a las Maldivas</title>
   </head>
+
   <body>
     <?php include("../includes/header.php"); ?>
     <main>
-      <section>
+      <section class="row">
         <article  id="allfather" class="container-fluid">
           <div class="row backgroundphoto w-100">
-
+            <div id="centertext" class="text-center col-12">
+              <h1 class="title">Las Maldivas</h1>
+            </div>
           </div>
+        </article>
 
+        <article id="bodyDiv" class="container-fluid">
+          <div class="row">
+            <div class="col-6">
+              <h2> Información sobre el viaje: </h2>
+
+
+
+              <ul>
+
+                <li>
+                  <h6> Mejor epoca del año: </h6>
+                   <?=$infotrip["bestseason"];?>
+                </li>
+                  </br>
+                <li>
+                  <h6> Tiempo ideal:</h6>
+                  <?=$infotrip["tiempoideal"];?>
+                </li>
+                  </br>
+                <li>
+                <h6>  Donde queda?</h6>
+                  <?=$infotrip["lugar"];?>
+                </li>
+                  </br>
+                <li>
+                  <h6>Descripción corto: </h6>
+                  <?=$infotrip["infolugar"];?>
+                </li>
+                  </br>
+              </ul>
+
+            </div>
+            <div class="col-6 rightside">
+              <h4>Posición:</h4>
+              <div class="mapouter"><div class="gmap_canvas"><iframe width="550" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Constance%20Halaveli&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/nordvpn-coupon-code/">code vpn</a></div></div>
+              
+
+            </div>
+          </div>
         </article>
       </section>
     </main>
@@ -31,3 +78,4 @@
 
   </body>
 </html>
+<?php }?>
