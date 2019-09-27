a4f6f201a381a45ff5739fe9afe6df7d5c892ab9
Leobag <leonard.bagiu@gmail.com>
2019-09-27 20:19:09 -0300

    sprint 1: Draft 1 producto

--- producto/info_producto/info_destinos.php
+++ producto/info_producto/info_destinos.php
@@ -4,11 +4,19 @@ $destinos = [
 "maldivas" => [
 
   "destino" => "Las Maldivas",
-"bestseason" => "Desde Noviembre hasta Marzo",
+  "preciofijo" => "3000 USD por persona",
+"bestseason" => "Clima tropical todo el año, hay dos temporadas de lluvia durante el año. La primera entre Mayo-Octubre
+y la segunda desde Noviembre hasta Abril.",
 "destinos" => 2,
 "tiempoideal" => "5 Noches",
 "hospedaje" => "Constance Halaveli",
 "lugar" => "45 minutos en barco de Malé",
+"zonatiempo" => "GMT+5",
+
+"descripcioncorto" => "Nuestro idea es ayudar en hacer cada viaje mas comodo posible con nuestro excelente servicio.
+Pensamos que se puede disfrutar cada lugar del mundo mucho mas si lo podes ver sin tener que preocuparse por el proximo parte
+del agenda del dia. Vamos a crear un viaje personalizado para Usted para que pueda ver el lugar de sus sueños y crear memorias por el resto de la vida",
+
 "infolugar" => "Hemos eligido Constance Halaveli como el hotel
 ideal para quedarse en Las Maldivas. Se van a quedar sobre el
 agua en las halucinantes casas sobre el agua, con servicio de gimnasio,
@@ -17,14 +25,17 @@ cortes de ténis. Cada casa viene equipado con televisór, Wi-Fi y cargadores de
 Ademas todo esto hay posibilidades de irse con muchas excursiones cerca al hotel.
 Los mas populares son: irse a otra isla para probar surfear, busear cerca los arrecifes de Las Maldivas y visitar restaurantes en islas distintas.",
 
-"incluido" => ["Transporte del aeropuerto al hotel", "Chip de celular local con 20GB de surf"],
+"incluido" => ["Transporte del aeropuerto al hotel", "Chip de celular local con 10GB de surf", "Servicio personalizado", "Transporte entre islas con lancha rapida"],
 
-"adicional" => ["Jet Privado", "Viaje privado desde tu casa al hotel", "Actividades del hotel"],
+"Personalizacion" => ["Eligir top 3 actividades cerca del hotel", "Eligir que cuarto del hotel", "Cual incenso en el cuarto al llegar",
+ "Estos son ejemplos, claramente vamos a intentar hacer cualquier cosa posible para ocomodar a Ustedes."],
 
-"jetprivado" => ["Masaje", "Champagne", "Cena de 3 Pasos", "Cambio de ropa para dormir", "Un peluche"],
+"adicional" => ["Jet Privado", "Viaje privado desde tu casa al aeropuerto", "Extra actividades del hotel"],
+
+"jetprivado" => ["Masaje", "Champagne", "Cena de 3 Pasos", "Cambio de ropa para dormir"],
 
 
-],
+]
 
 ];
 
--- producto/maldivas.php
+++ producto/maldivas.php
@@ -15,34 +15,63 @@ if($destinos["maldivas"]){
     <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Raleway&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/34b9ea8fdc.js"></script>
     <link rel="stylesheet" href="../includes/general.css">
-    <link rel="stylesheet" href="css/allstyles.css">
+    <link rel="stylesheet" href="css/allstyle.css">
     <title>Viajá a las Maldivas</title>
   </head>
 
   <body>
     <?php include("../includes/header.php"); ?>
     <main>
-      <section class="row">
+      <section class="mx-1 row">
         <article  id="allfather" class="container-fluid">
           <div class="row backgroundphoto w-100">
             <div id="centertext" class="text-center col-12">
-              <h1 class="title">Las Maldivas</h1>
+              <h1 class="title">Maldivas</h1>
             </div>
           </div>
         </article>
 
-        <article id="bodyDiv" class="container-fluid">
-          <div class="row">
-            <div class="col-6">
-              <h2> Información sobre el viaje: </h2>
 
 
+        <article id="buttons" class="container my-2">
+              <div id="buttongrp"class="row">
+                <button class="btn btn-primary col-3 in" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="#collapse1">
+                  GENERAL
+                </button>
 
-              <ul>
+                <button class="btn btn-primary col-3" type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="#collapse2">
+                  SOBRE EL VIAJE
+                </button>
+                <button class="btn btn-primary col-3" type="button"  data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                  MAS FOTOS
+                </button>
+                <button class="btn btn-primary col-3" type="button"  data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                  RESERVAR
+                </button>
+              </div>
+
+          </article>
+
+
+        <article id="aboutDiv" class="collapse container-fluid p-0">
+          <div id="collapse1" class="collapse show row reverse" data-parent="#buttongrp">
+
+            <div class="col-12 col-md-6 rightside mt-1 py-0">
+              <h4>Posición:</h4>
+              <div class="mapouter">
+                <div class="gmap_canvas">
+                  <iframe  height="400" width="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=Constance%20Halaveli&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-5 paragraph">
+              <h2> Información general: </h2>
+              <ul class="px-1">
 
                 <li>
-                  <h6> Mejor epoca del año: </h6>
-                   <?=$infotrip["bestseason"];?>
+                  <h6>Descripción corto: </h6>
+                  <?=$infotrip["infolugar"];?>
                 </li>
                   </br>
                 <li>
@@ -55,27 +84,87 @@ if($destinos["maldivas"]){
                   <?=$infotrip["lugar"];?>
                 </li>
                   </br>
+
                 <li>
-                  <h6>Descripción corto: </h6>
-                  <?=$infotrip["infolugar"];?>
+                  <h6> Mejor epoca del año: </h6>
+                   <?=$infotrip["bestseason"];?>
                 </li>
-                  </br>
+              </br>
               </ul>
 
             </div>
-            <div class="col-6 rightside">
-              <h4>Posición:</h4>
-              <div class="mapouter"><div class="gmap_canvas"><iframe width="550" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Constance%20Halaveli&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/nordvpn-coupon-code/">code vpn</a></div></div>
-              
 
-            </div>
           </div>
         </article>
+
+        <article id="incluidoDiv" class="container-fluid">
+          <div id="collapse2" class="row collapse" data-parent="#buttongrp">
+            <div class="col-md-1">
+              <span></span>
+            </div>
+            <div class="bar2headline col-12 col-md-7">
+                <h2 class="mb-1">Viajar con nosotros</h2>
+                <p><?=$infotrip["descripcioncorto"]?></p>
+
+                <h3>Personalizacion posible:</h3>
+                <ul class="list-style pl-1">
+                <?php for($i=0; $i < count($infotrip["Personalizacion"]); $i++){
+                  $trip = $infotrip["Personalizacion"];
+                  ?>
+                  <li>
+                    <i class="fas fa-plane"></i>
+                    <?= $trip[$i]; ?>
+                  </li>
+                <?php } ?>
+                </ul>
+
+            </div>
+              <div class="bar2headline col-12 col-md-3">
+                <h3>Incluido en el viaje:</h3>
+                <ul class="list-style pl-1">
+
+                  <?php for($i=0; $i < count($infotrip["incluido"]); $i++){
+                    $trip = $infotrip["incluido"];
+
+                    ?>
+                    <li>
+                    <i class="fas fa-plane"></i>
+                      <?= $trip[$i]; ?>
+                    </li>
+                  <?php } ?>
+                </ul>
+              </br>
+
+
+
+
+                  <h3>Si viajas por jet privado el viaje incluye: </h3>
+
+                  <ul class="list-style pl-1">
+
+                    <?php for($i=0; $i < count($infotrip["jetprivado"]); $i++){
+                      $trip = $infotrip["jetprivado"];
+
+                      ?>
+                      <li>
+                      <i class="fas fa-plane"></i>
+                        <?= $trip[$i]; ?>
+                      </li>
+                    <?php } ?>
+                  </ul>
+            </div>
+
+          </div>
+        </article>
+
       </section>
     </main>
 
     <?php include("../includes/footer.php"); ?>
 
+<?php } ?>
   </body>
 </html>
-<?php }?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
