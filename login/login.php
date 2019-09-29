22c968d52aec72e7e6c73943d51fcb6438fe2281
Leobag <leonard.bagiu@gmail.com>
2019-09-29 11:57:24 -0300

    CSS general cambiar nombre

--- login/login.php
+++ login/login.php
@@ -4,14 +4,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Raleway&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../includes/general.css">
+    <link rel="stylesheet" href="../includes/general1.css">
     <link rel="stylesheet" href="CSS/login.css">
     <script src="https://kit.fontawesome.com/34b9ea8fdc.js"></script>
     <title>LUSH - Log in</title>
   </head>
   <body>
     <?php include("../includes/header.php"); ?>
-    
+
     <div class="foto container-fluid row pl-5 pt-5">
       <div class="col-8 col-md-5 col-lg-4 offset-1 pt-5">
         <form>
