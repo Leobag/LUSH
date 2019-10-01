506f210cfed3583e5f3e0583ca8808037f501c80
Leobag <leonard.bagiu@gmail.com>
2019-10-01 20:18:22 -0300

    cree pagina About

---
+++ about/nosotros.php
@@ -0,0 +1,91 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../includes/general1.css">
+    <link rel="stylesheet" href="CSS/about.css">
+    <script src="https://kit.fontawesome.com/34b9ea8fdc.js"></script>
+    <title>LUSH - Nosotros</title>
+  </head>
+  <body>
+
+    <?php include("../includes/header.php"); ?>
+
+    <section id="main" class="container-fluid">
+      <div class="row">
+        <div class="col-12 text-center pt-5 mt-3">
+          <h1>nuestra compania</h1>
+          </div>
+          <div class="feature_divider">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="feature_divider_small">
+          </div>
+         </div>
+         <div class="row">
+           <article class="texto col-12 text-center">
+             <h2>our history</h2>
+             <div class="feature_divider_small">
+             </div>
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+           </article>
+          </div>
+          <div class="feature_divider_small">
+          </div>
+          <div class="row">
+            <article class="texto col-12 text-center">
+              <h2>our team</h2>
+              <div class="feature_divider_small">
+              </div>
+            </article>
+          </div>
+          <div class="team row">
+            <div class="col-12 col-md-12 col-lg-4 text-center">
+              <img class="imagen" src="img/team.png" alt="foto">
+              <h3>Patricio Baptista</h3>
+              <div class="feature_divider_small">
+              </div>
+              <p>Director de Marketing</p>
+            </div>
+            <div class="col-12 col-md-12 col-lg-4 text-center">
+              <img class="imagen" src="img/team.png" alt="foto">
+              <h3>Martin Sozzi</h3>
+              <div class="feature_divider_small">
+              </div>
+              <p>Director de Marketing</p>
+            </div>
+            <div class="col-12 col-md-12 col-lg-4 text-center">
+              <img class="imagen" src="img/team.png" alt="foto">
+              <h3>Leo Bagiu</h3>
+              <div class="feature_divider_small">
+              </div>
+              <p>Director de Marketing</p>
+            </div>
+            <div class="feature_divider_small">
+            </div>
+           </div>
+           <div class="row">
+             <article class="texto col-12 text-center">
+               <h2>our mission</h2>
+               <div class="feature_divider_small">
+               </div>
+               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+             </article>
+            </div>
+      </section>
+
+    <?php include("../includes/footer.php"); ?>
+
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
