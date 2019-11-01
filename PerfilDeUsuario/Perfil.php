dc695c22b99f4e10c1a1d56e681b1945a0dfddfd
MartinSozzi <martinsozzi1998@gmail.com>
2019-11-01 03:57:17 -0300

    correccion perfil

--- PerfilDeUsuario/Perfil.php
+++ PerfilDeUsuario/Perfil.php
@@ -1,59 +1,53 @@
 <!DOCTYPE html>
-<html lang="es" dir="ltr">
+<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <title>LUSH - Tu Perfil</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Raleway&display=swap" rel="stylesheet">
    <script src="https://kit.fontawesome.com/34b9ea8fdc.js"></script>
    <link rel="stylesheet" href="../includes/general1.css">
-   <link rel="stylesheet" href="css/Perfil.css">
-   <title>LUSH - Tu Perfil</title>
+   <link rel="stylesheet" href="responsive2.css">
   </head>
-  <body class="body">
+  <body>
 
-    <?php include("../includes/header.php"); ?>
-    
-    <div id="falsoheader">
-      <img src="img/foto.jpg" alt="perfil" id="foto">
-        <h1 id="nombre">Martin Sozzi</h1>
-          <h2 id="nacionalidad">Argentina</h2>
+<?php include("../includes/header.php"); ?>
 
-    <nav>
-      <ul class="menu">
-        <li id="menu">Informacion</li>
-        <li id="menu">Reservas</li>
-      </ul>
-    </nav>
-    </div>
+<div class="row">
+  <div class="falsoheader col-sm-6">
+    <img src="img/foto.jpg" alt="perfil" class="foto">
+      <h1 class="nombre">Martin Sozzi</h1>
+        <h2 class="nacionalidad">Argentina</h2>
+          </div>
+  <div class="londres col-sm-6">
+    <img src="img/londres.jpg" alt="foto" class="viaje1">
+    <h3>Ultimo Viaje</h3>
+     <h4>Londres</h4>
+       <h5>Puente de la Torre</h5>
+        <h6>17 - 27 Agosto, 2017</h6>
+          </div>
+</div>
 
-    <div class="informacion">
-     <ul>
-       <li id="informacion">Edad:</li>
-       <li id="informacion">Direccion:</li>
-       <li id="informacion">Email:</li>
-       <li id="informacion">Celular:</li>
-     </ul>
-     <ul>
-       <li id="datos">29</li>
-       <li id="datos">Buenos Aires</li>
-       <li id="datos">ejemplo@gmail.com</li>
-       <li id="datos">54 9 11 0234 5678</li>
-     </ul>
-    </div>
+<div class="informacion row">
+  <div class="col-sm-12">
+    <ul>
+      <li id="informacion">Edad:</li>
+      <li id="informacion">Direccion:</li>
+      <li id="informacion">Email:</li>
+      <li id="informacion">Celular:</li>
+    </ul>
+    <ul>
+      <li id="datos">29</li>
+      <li id="datos">Buenos Aires</li>
+      <li id="datos">ejemplo@gmail.com</li>
+      <li id="datos">54 9 11 0234 5678</li>
+    </ul>
+  </div>
 
-    <div class="fusion">
-      </div>
+</div>
 
-    <div class="londres">
-      <img src="img/londres.jpg" alt="foto" id="viaje1">
-       <h4>Londres</h4>
-         <h5>Puente de la Torre</h5>
-          <h6>17 - 27 Agosto, 2017</h6>
-    </div>
+<?php include("../includes/footer.php"); ?>
 
-
-  <?php include("../includes/footer.php"); ?>
-
-  </body>
+ </body>
 </html>
